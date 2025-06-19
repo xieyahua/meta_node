81aefe8b7b507764fc6a9c3521eef3da7f6541ed
xieyahua <138201509+xieyahua@users.noreply.github.com>
2025-06-19 15:52:52 +0800

    Update tast1.go

--- tast1.go
+++ tast1.go
@@ -25,4 +25,21 @@ func Task1() {
 }
 
 //回文数
-
+package main
+import (
+	"fmt"
+)
+func isPalindrome(x int) bool {
+    // y := len(strconv.Itoa(x)) -1 //判断数字有几位数
+    y := x
+    m := 0 
+    for y>0 {
+        m = m*10 + y%10
+        y = y/10
+    }
+    if m == x {
+        return true
+    } else {
+        return false
+    }
+}
