05cbc1c9ba4928180c303867da8e9bd824be85c5
xieyahua <138201509+xieyahua@users.noreply.github.com>
2025-06-19 15:52:15 +0800

    Update tast1.go

--- tast1.go
+++ tast1.go
@@ -1,1 +1,28 @@
 
+// 只出现一次的数字
+package main
+
+import (
+	"fmt"
+)
+func Task1() {
+	method := func(nums []int) int {
+		numCount := make(map[int]int)
+		for _, num := range nums {
+			numCount[num]++
+		}
+
+		for num, count := range numCount {
+			if count == 1 {
+				return num
+			}
+		}
+
+		panic("no unique element found")
+	}
+	nums := []int{2, 2, 1}
+	fmt.Println(method(nums))
+}
+
+//回文数
+
