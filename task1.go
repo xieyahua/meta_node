5267e9d067c03c2178eacde24bfcf366cac02bc0
xieyahua <138201509+xieyahua@users.noreply.github.com>
2025-06-20 09:30:45 +0800

    Update task1.go

--- task1.go
+++ task1.go
@@ -1,5 +1,5 @@
 
-// 只出现一次的数字
+// 1.只出现一次的数字
 package main
 
 import (
@@ -24,7 +24,7 @@ func Task1() {
 	fmt.Println(method(nums))
 }
 
-//回文数
+//2.回文数
 package main
 import (
 	"fmt"
@@ -42,4 +42,31 @@ func isPalindrome(x int) bool {
     } else {
         return false
     }
+}
+
+// 3.有效的括号
+func isValid(s string) bool {
+    n := len(s)
+    if n%2 == 1 {
+        return false
+    }
+    
+    pairs := map[byte]byte{
+        ')': '(',
+        ']': '[',
+        '}': '{',
+    }
+    
+    stack := []byte{}
+    for i := 0; i < n; i++ {
+        if pairs[s[i]] > 0 {
+            if len(stack) == 0 || stack[len(stack)-1] != pairs[s[i]] {
+                return false
+            }
+            stack = stack[:len(stack)-1]
+        } else {
+            stack = append(stack, s[i])
+        }
+    }
+    return len(stack) == 0
 }
