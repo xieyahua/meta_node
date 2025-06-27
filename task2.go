11ece1caf1d62667ca0badbc220f37486ec18784
xieyahua <138201509+xieyahua@users.noreply.github.com>
2025-06-27 11:21:57 +0800

    Update task2.go

--- task2.go
+++ task2.go
@@ -375,3 +375,34 @@ func main() {
 	wg.Wait()
 	fmt.Printf("最终计数器值: %d\n", counter)
 }
+
+
+---------------------------------------------锁机制2---------------------------------------------------------------
+
+
+package main
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
+
+func main() {
+	var counter int64
+	var wg sync.WaitGroup
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for j := 0; j < 1000; j++ {
+				atomic.AddInt64(&counter, 1)
+			}
+		}()
+	}
+
+	wg.Wait()
+	fmt.Printf("最终计数器值: %d\n", counter)
+}
+
